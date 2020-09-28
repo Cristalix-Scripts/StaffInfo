501d7bfe00e96dfe592e3f6cb1e49537e8606847
Nartsissov <54633007+Nartsissov@users.noreply.github.com>
2020-09-28 15:15:08 +0300

    Removed unused variables. Added rainbow colors support for keystrokes.

--- StaffInfoKeystrokes.ts
+++ StaffInfoKeystrokes.ts
@@ -38,11 +38,10 @@ var allTime = config.allTime
 var timestamp = 0
 var currentTime = 0
 
-var leftClicks = 0
-let rightClicks = 0
-
 scheduler.scheduleAtFixedRate(function() {
     if (timestamp == 0) return
+    leftClicks = 0
+    rightClicks = 0
     config.allTime = allTime
     Config.save(nickname, config)
 }, 0, 10, TimeUnit.SECONDS)
@@ -83,27 +82,29 @@ function buildBoard() {
 
 function buildKeystrokes() {
     if (isKeystrokesEnabled) {
+        let color = 0xCFCCC2
+        let pressColor = 0x606C6E
         if (isKeystrokesRainbow) color = Colors.HSBtoRGB(Math.ceil(System.currentTimeMillis() / 20) % 360 / 360, 1, 1)
         Draw.drawRect(50, 105, 30, 125, 0xCC000000)
-        Draw.drawString('§'.concat(Keyboard.isKeyDown(31) ? 'a' : 'c').concat('S'), 38, 112, 0)
+        Draw.drawString('S', 38, 112, Keyboard.isKeyDown(31) ? pressColor : color)
 
         Draw.drawRect(50, 80, 30, 100, 0xCC000000)
-        Draw.drawString('§'.concat(Keyboard.isKeyDown(17) ? 'a' : 'c').concat('W'), 38, 87, 0)
+        Draw.drawString('W', 38, 87, Keyboard.isKeyDown(17) ? pressColor : color)
 
         Draw.drawRect(75, 105, 55, 125, 0xCC000000)
-        Draw.drawString('§'.concat(Keyboard.isKeyDown(32) ? 'a' : 'c').concat('A'), 63, 112, 0)
+        Draw.drawString('A', 63, 112, Keyboard.isKeyDown(32) ? pressColor : color)
 
         Draw.drawRect(25, 105, 5, 125, 0xCC000000)
-        Draw.drawString('§'.concat(Keyboard.isKeyDown(30) ? 'a' : 'c').concat('D'), 13, 112, 0)
+        Draw.drawString('D', 13, 112, Keyboard.isKeyDown(30) ? pressColor : color)
 
         Draw.drawRect(75, 130, 5, 150, 0xCC000000)
-        Draw.drawString('§'.concat(Keyboard.isKeyDown(57) ? 'a' : 'c').concat('SPACE'), 26, 137, 0)
+        Draw.drawString('SPACE', 26, 137, Keyboard.isKeyDown(57) ? pressColor : color)
 
         Draw.drawRect(37, 155, 5, 175, 0xCC000000)
-        Draw.drawString('§'.concat(Mouse.isButtonDown(0) ? 'a' : 'c').concat(leftClicks == 0 ? 'LMB' : leftClicks), (leftClicks == 1 ? 7 : 13), 162, 0)
+        Draw.drawString('LMB', 13, 162, Mouse.isButtonDown(0) ? pressColor : color)
 
         Draw.drawRect(75, 155, 42, 175, 0xCC000000)
-        Draw.drawString('§'.concat(Mouse.isButtonDown(1) ? 'a' : 'c').concat(rightClicks == 0 ? 'RMB' : rightClicks), (rightClicks == 0 ? 50 : 56), 162, 0)
+        Draw.drawString('RMB', 50, 162, Mouse.isButtonDown(1) ? pressColor : color)
 
     }
 }
